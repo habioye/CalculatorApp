e22b597aa25cd3771d515166db72e4f800fd90c7
habioye <78454514+habioye@users.noreply.github.com>
2024-06-29 22:50:27 -0500

    removed erronious component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
