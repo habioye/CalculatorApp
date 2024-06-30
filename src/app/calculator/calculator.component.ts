7c5a892184ecd82d728b4f189423e8cd8582f298
habioye <78454514+habioye@users.noreply.github.com>
2024-06-29 22:48:38 -0500

    added the calculator as title

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -12,15 +12,38 @@ export class CalculatorComponent {
   displayarr2: string[] = [];
   first = true;
   state = 1;
+  invalidexpression = false;
   public addnumber(input:string): void {
+    this.invalidexpression = false;
     switch(this.state) {
       case 1:
+        this.state = 2;
+        this.displayarr = [];
+        this.displayarr2 = [];
+        this.displayarr.push(input);
+        this.display = this.displayarr.join("");
         break;
-        case 2:
-          break;
-        case 3:
-          break;
-        
+      case 2:
+        this.displayarr.push(input);
+        this.display = this.displayarr.join("");
+        break;
+      case 3:
+        this.state = 4;
+        this.displayarr2.push(input);
+        this.display = this.displayarr2.join("");
+        break;
+      case 4:
+        this.displayarr2.push(input);
+        this.display = this.displayarr2.join("");
+        break;
+      case 5:
+        this.state = 2;
+        this.display = input;
+        this.displayarr = [input];
+        this.displayarr2 = [];
+
+        break;
+      
     }
     // console.log("qwert out");
     // if (this.first) {
@@ -33,21 +56,82 @@ export class CalculatorComponent {
 
   }
   operate(input:string): void {
-    if (this.operator == '') { // adding first operator
+    if (this.invalidexpression) return;
     this.operator = input;
-    this.first = !this.first;
-    } else { // chaining operators
-      this.evaluate();
-      this.operator = '';
-      this.displayarr = []
+    // if (this.state == 1) {
+    //   this.displayarr = ['0'];
+    // }
+    // this.state = 3;
+    switch(this.state) {
+      case 1:
+        this.state = 3;
+        this.displayarr = ['0'];
+        
+
+        break;
+      case 2:
+        this.state = 3
+
+        break;
+      case 3:
+        break;
+      case 4:
+        this.evaluate();
+        if (this.display == "Invalid Expression") {
+          this.state = 2;
+          this.displayarr = [];
+
+        }
+        this.operate(this.operator);
+            // try{
+    // this.display = eval(this.displayarr.join("")+this.operator+this.displayarr2.join("")).toString();
+    // } catch (error) {
+    //   this.display = "Invalid Expression";
+    // }
+        break;
+      case 5:
+
+        break;
+      
     }
+    // if (this.operator == '') { // adding first operator
+    // this.operator = input;
+    // this.first = !this.first;
+    // } else { // chaining operators
+    //   this.evaluate();
+    //   this.operator = '';
+    //   this.displayarr = []
+    // }
   }
   evaluate():void {
-    try{
-    this.display = eval(this.displayarr.join("")+this.operator+this.displayarr2.join("")).toString();
-    } catch (error) {
-      this.display = "Invalid Expression";
+    if (this.invalidexpression) return;
+    switch(this.state) {
+      case 1:
+        this.state = 2;
+        this.displayarr = [];
+        this.displayarr2 = [];
+        this.display = this.displayarr.join("");
+        break;
+      case 2:
+        this.state = 5;
+        this.operator = "+";
+        this.displayarr2 = ['0'];
+        break;
+      case 3:
+        this.state = 5;
+        this.displayarr2 = Object.assign([],this.displayarr);
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      
     }
+    // try{
+    // this.display = eval(this.displayarr.join("")+this.operator+this.displayarr2.join("")).toString();
+    // } catch (error) {
+    //   this.display = "Invalid Expression";
+    // }
 
   }
 
